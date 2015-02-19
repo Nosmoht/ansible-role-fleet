afb0ee84f7561f87c3bf85d02cf1372e60852400
tkrah <thomas.krahn@esailors.de>
2015-02-19 19:23:55 +0100

    Add submitting of unit files

--- library/fleet_unit.py
+++ library/fleet_unit.py
@@ -6,26 +6,70 @@ import os
 
 FLEETCTL_BIN = '/usr/bin/fleetctl'
 
+def fleetctl(module, action, unit):
+    command = "%s %s %s" % (FLEETCTL_BIN, action, unit)
+    rc, out, err = module.run_command(command)
+    if rc != 0:
+        module.fail_json(msg="Error while %s unit file %s: %s" % (action, unit, out), rc=rc, err=err)
+    return True
+
+def submit_unit(module, name, path):
+    return fleetctl(module, 'submit', path + '/' + name)
+
+def load_unit(module, name):
+    return fleetctl(module, 'load', name)
+    
+def start_unit(module, name):
+    return fleetctl(module, 'start', name)
+    
+def stop_unit(module, name):
+    return fleetctl(module, 'stop', name)
+    
+def unload_unit(module, name):
+    return fleetctl(module, 'unload', name)
+    
+def destroy_unit(module, name):
+    return fleetctl(module, 'destroy', name)
+    
 def get_unit_files(module):
     command = "%s list-unit-files" % (FLEETCTL_BIN)
     rc, out, err = module.run_command(command)
     if rc != 0:
         module.fail_json(msg="Error while listing unit files: %s" % (out), rc=rc, err=err)
-    return True, out
+        
+    result = [re.sub(r'(\t+?)\1', r'\1', line).split('\t') for line in out.split('\n') if line]        
+    return result[1:]
+
+def is_unit_submitted(unit, unit_files):
+    for line in unit_files:
+        if line[0] == unit:
+            return True
+    return False
 
 def ensure(module):
     changed = False
+    unit_files = None
+    unit_file_exists = False
+    instance_unit = False
     # Set parameters
     name = module.params['name']
+    path = module.params['path']
     state = module.params['state']
-    # Execute tasks based on current state
+    # Determine if unit is an instance unit (http://0pointer.de/blog/projects/instances.html)
+    instance_unit = name.find('@') != -1
     unit_files = get_unit_files(module)
+    if not instance_unit:
+        unit_submitted = is_unit_submitted(name, unit_files)
+    if state != 'destroyed' and not unit_submitted:        
+        changed = submit_unit(module, name, path)
+#    if state == 'loaded' 
     return changed
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            name=dict(required=True, Type=String),
+            name=dict(required=True, Type='String'),
+            path=dict(required=False, Type='String'),
             state=dict(Default='started', choices=['submitted', 'loaded', 'started', 'stopped', 'unloaded', 'destroyed']),
         ),
     )
@@ -33,6 +77,6 @@ def main():
     changed = ensure(module)
     module.exit_json(changed=changed, name=module.params['name'])
 
-# this is magic, see lib/ansible/module_common.py
-# <<INCLUDE_ANSIBLE_MODULE_COMMON>>
+# import module snippets
+from ansible.module_utils.basic import *
 main()
