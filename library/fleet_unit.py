5ec5fb9a9977d92b47d7099850d18b0ead453a22
tkrah <thomas.krahn@esailors.de>
2015-02-20 15:43:52 +0100

    Change handling of unit-files and units. Implement stopping of units.

--- library/fleet_unit.py
+++ library/fleet_unit.py
@@ -41,10 +41,32 @@ def get_unit_files(module):
     return result[1:]
 
 def is_unit_submitted(unit, unit_files):
+    unit_file_name = None
+    # Determine if unit is an instance unit (http://0pointer.de/blog/projects/instances.html)
+    # True: apache@8080.service, False: apache@.service, myapp.service
+    if re.search(r'@.+\.', unit):
+        unit_file_name = re.sub('(.+@)(.+)(\..+)', r'\1\3', unit)
+    else:
+        unit_file_name = unit;
+    
     for line in unit_files:
-        if line[0] == unit:
+        if line[0] == unit_file_name:
             return True
     return False
+
+def get_unit_state(unit):
+    if unit:
+        return unit[3]
+    return None
+
+def unit_is_loaded(unit):
+    return get_unit_state(unit) == 'loaded'
+
+def unit_is_started(unit):
+    return get_unit_state(unit) == 'running'
+
+def unit_is_stopped(unit):
+    return get_unit_state(unit) == 'stop'
 
 def get_units(module):
     command = "%s list-units" % (FLEETCTL_BIN)
@@ -54,24 +76,38 @@ def get_units(module):
     result = [re.sub(r'(\t+?)\1', r'\1', line).split('\t') for line in out.split('\n') if line]
     return result[1:]
 
+def get_unit(name, units):
+    for unit in units:
+        if unit[0] == name:
+            return unit;
+    return None
+
 def ensure(module):
     changed = False
     unit_files = None
     unit_file_exists = False
-    instance_unit = False
+    unit = None
     # Set parameters
     name = module.params['name']
     path = module.params['path']
     state = module.params['state']
-    # Determine if unit is an instance unit (http://0pointer.de/blog/projects/instances.html)
-    instance_unit = name.find('@') != -1
     unit_files = get_unit_files(module)
-    if not instance_unit:
-        unit_submitted = is_unit_submitted(name, unit_files)
-    if state != 'destroyed' and not unit_submitted:        
-        changed = submit_unit(module, name, path)
+    unit_submitted = is_unit_submitted(name, unit_files)
+ 
+    if not unit_submitted:
+        if state not in ['destroyed', 'unloaded']: 
+            changed = submit_unit(module, name, path)
+        else:
+            module.fail_json(msg="Unit not submitted: %s" % name)
+
+    unit = get_unit(name, get_units(module))
+
+    if state == 'started' and not unit_is_started(unit):
+        changed = start_unit(module, name)
+    if state == 'stopped' and unit_is_started(unit):
+        changed = stop_unit(module, name)
     if state == 'destroyed' and unit_submitted:
-        changed = destroy_unit(module, name)
+        changed = destroy_unit(module, unit)
     return changed
 
 def main():
