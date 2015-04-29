6eef729d28bbea6a7d157144788c8663d60c211c
tkrah <thomas.krahn@esailors.de>
2015-04-29 19:07:54 +0200

    Add support of defining a path and handle all files inside with one call to fleetctl

--- library/fleet_unit.py
+++ library/fleet_unit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
 
 import os
@@ -7,51 +6,31 @@ import re
 
 FLEETCTL_BIN = 'fleetctl'
 
-FLEET_MATRIX = [{"command": "submit", "state": "inactive"},
-                {"command": "load", "state": "loaded"},
-                {"command": "start", "state": "launched"},
-                {"command": "stop", "state": "loaded"},
-                {"command": "unload", "state": "inactive"}]
+FLEET_MATRIX = [{'ensure_state': 'submitted', 'unit_state': 'inactive', 'command': 'submit'},
+                {'ensure_state': 'loaded', 'unit_state': 'loaded', 'command': 'load'},
+                {'ensure_state': 'started', 'unit_state': 'launched', 'command': 'start'},
+                {'ensure_state': 'stopped', 'unit_state': 'loaded', 'command': 'stop'},
+                {'ensure_state': 'unloaded', 'unit_state': 'inactive', 'command': 'unload'}]
 
-def fleetctl(module, action, unit):
-    command = "%s %s %s" % (FLEETCTL_BIN, action, unit)
-    rc, out, err = module.run_command(command)
+
+def fleetctl(command, command_options='', global_options='', arguments=''):
+    cmd = '{fleetctl} {global_options} {command} {command_options} {arguments}'.format(fleetctl=FLEETCTL_BIN,
+                                                                                       global_options=global_options,
+                                                                                       command=command,
+                                                                                       command_options=command_options,
+                                                                                       arguments=arguments)
+    rc, out, err = module.run_command(cmd)
     if rc != 0:
-        module.fail_json(msg="Error while %s unit file %s: %s" % (action, unit, out), rc=rc, err=err)
-    return True
+        module.fail_json(msg='Error while executing "{cmd}": {error}'.format(cmd=cmd, error=err))
+    return out
 
-def submit_unit(module, name, path):
-    if path:
-        return fleetctl(module, 'submit', path + '/' + name)
-    else:
-        return fleetctl(module, 'submit', name)
 
-def load_unit(module, name):
-    return fleetctl(module, 'load', name)
+def get_fleet_units():
+    out = fleetctl(command='list-unit-files', arguments='--no-legend')
+    return [re.sub(r'(\t+?)\1', r'\1', line).split('\t') for line in out.split('\n') if line]
 
-def start_unit(module, name):
-    return fleetctl(module, 'start', name)
-
-def stop_unit(module, name):
-    return fleetctl(module, 'stop', name)
-
-def unload_unit(module, name):
-    return fleetctl(module, 'unload', name)
-
-def destroy_unit(module, name):
-    return fleetctl(module, 'destroy', name)
-
-def get_unit_files(module):
-    command = "%s list-unit-files" % (FLEETCTL_BIN)
-    rc, out, err = module.run_command(command)
-    if rc != 0:
-        module.fail_json(msg="Error while listing unit files: %s" % (out), rc=rc, err=err)
-
-    result = [re.sub(r'(\t+?)\1', r'\1', line).split('\t') for line in out.split('\n') if line]
-    return result[1:]
 
 def is_unit_submitted(unit, unit_files):
-    unit_file_name = None
     # Determine if unit is an instance unit (http://0pointer.de/blog/projects/instances.html)
     # True: apache@8080.service, False: apache@.service, myapp.service
     if re.search(r'@.+\.', unit):
@@ -64,71 +43,88 @@ def is_unit_submitted(unit, unit_files):
             return True
     return False
 
+
+def get_unit_name(unit):
+    return unit[0]
+
+
 def get_unit_state(unit):
     if unit:
         return unit[3]
     return None
 
-def unit_is_loaded(unit):
-    return get_unit_state(unit) == 'loaded'
-
-def unit_is_started(unit):
-    return get_unit_state(unit) == 'launched'
-
-def unit_is_stopped(unit):
-    return get_unit_state(unit) == 'loaded'
-
-def unit_is_unloaded(unit):
-    return get_unit_state(unit) == 'inactive'
 
 def get_unit(name, units):
-    for unit in units:
-        if unit[0] == name:
-            return unit
-    return None
+    result = None
+    if units:
+        for unit in units:
+            if get_unit_name(unit) == name:
+                result = unit
+                break
+    return result
 
-def ensure(module):
-    changed = False
-    unit = None
-    # Set parameters
-    name = os.path.basename(module.params['name'])
-    path = os.path.dirname(module.params['name'])
+
+def unit_has_desired_state(unit, state):
+    fleet_state = [item for item in FLEET_MATRIX if item.get('ensure_state') == state]
+    return fleet_state[0].get('unit_state') == get_unit_state(unit)
+
+
+def get_state_command(state):
+    return ' '.join([item.get('command') for item in FLEET_MATRIX if item.get('ensure_state') == state])
+
+
+def ensure():
+    name = module.params['name']
+    path = module.params['path']
     state = module.params['state']
 
-    units = get_unit_files(module)
-    unit_submitted = is_unit_submitted(name, units)
+    if path:
+        try:
+            unit_files = [os.path.join(path, filename) for filename in os.listdir(path)]
+        except OSError as e:
+            module.fail_json(msg='Could not list files in {path}: {error}'.format(path=path, error=e.strerror))
+    else:
+        unit_files = [os.path.join(os.path.dirname(name), os.path.basename(name))]
 
-    if not unit_submitted:
-        if state not in ['destroyed', 'unloaded']:
-            changed = submit_unit(module, name, path)
-        else:
-            module.fail_json(msg="Unit not submitted: %s" % name)
+    fleet_units = get_fleet_units()
+    unit_files_to_handle = list()
+    for item in unit_files:
+        unit = get_unit(os.path.basename(item), fleet_units)
+        if state == 'submitted' and unit:
+            continue
+        if state == 'destroyed' and not unit:
+            continue
+        if not unit_has_desired_state(unit, state):
+            unit_files_to_handle.append(item)
 
-    unit = get_unit(name, units)
+    if len(unit_files_to_handle) > 0:
+        fleet_command = get_state_command(state)
+        fleet_command_options = ' '.join([item for item in unit_files_to_handle])
+        # module.fail_json(msg='Command: {cmd}\nOptions: {opts}'.format(cmd=fleet_command, opts=fleet_command_options))
+        fleetctl(command=fleet_command,
+                 command_options=fleet_command_options)
+        return True, unit_files_to_handle
 
-    if state == 'loaded' and not unit_is_loaded(unit):
-        changed = load_unit(module, name)
-    if state == 'started' and not unit_is_started(unit):
-        changed = start_unit(module, name)
-    if state == 'stopped' and unit_is_started(unit):
-        changed = stop_unit(module, name)
-    if state == 'destroyed' and unit_submitted:
-        changed = destroy_unit(module, unit)
-    if state == 'unloaded' and not unit_is_unloaded(unit):
-        changed = unload_unit(module, name)
-    return changed
+    return False, unit_files_to_handle
+
 
 def main():
+    global module
     module = AnsibleModule(
         argument_spec=dict(
-            name=dict(required=True, Type='String'),
-            state=dict(Default='started', choices=['submitted', 'loaded', 'started', 'stopped', 'unloaded', 'destroyed']),
+            name=dict(type='str', default=None),
+            path=dict(type='str', default=None),
+            state=dict(type='str', default='started',
+                       choices=['submitted', 'loaded', 'started', 'stopped', 'unloaded', 'destroyed']),
         ),
+        required_one_of=[('name', 'path')],
+        supports_check_mode=True
     )
 
-    changed = ensure(module)
-    module.exit_json(changed=changed, name=module.params['name'])
+    changed, units = ensure()
+    module.exit_json(changed=changed, units=units)
 
 # import module snippets
 from ansible.module_utils.basic import *
+
 main()
